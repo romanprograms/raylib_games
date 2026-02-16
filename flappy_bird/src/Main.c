95a815c402233d38bf9bc952beccb4dc8503d96a
Roman Nikolaenkov <romanliveshere@gmail.com>
2026-02-15 17:57:07 -0800

    Draw a ball and move it with awsd keys

--- flappy_bird/src/Main.c
+++ flappy_bird/src/Main.c
@@ -1,24 +1,36 @@
 #include "raylib.h"
 #include <stdio.h>
 
-int main(void) {
+int main(void)
+{
   // Initialization
   const int screenWidth = 800;
   const int screenHeight = 450;
 
   InitWindow(screenWidth, screenHeight, "Flappy Bird made with Raylib");
-  SetTargetFPS(60);
 
+  Vector2 ballPosition = {(float)screenWidth / 2.0f, (float)screenHeight / 2.0f};
+
+  SetTargetFPS(60);
   // Main game loop
-  while (!WindowShouldClose()) {
+  while (!WindowShouldClose())
+  {
     // Update
-    // TODO: update your variable here
+    if (IsKeyDown(KEY_D))
+      ballPosition.x += 2.0f;
+    if (IsKeyDown(KEY_A))
+      ballPosition.x -= 2.0f;
+    if (IsKeyDown(KEY_W))
+      ballPosition.y -= 2.0f;
+    if (IsKeyDown(KEY_S))
+      ballPosition.y += 2.0f;
 
     // Draw
     BeginDrawing();
     ClearBackground(RAYWHITE);
-    DrawText("This is where Flappy Bird Game will be drawn", 190, 200, 20,
-             LIGHTGRAY);
+    DrawText("This is where Flappy Bird Game will be drawn", 10, 10, 20,
+             DARKGRAY);
+    DrawCircleV(ballPosition, 50, MAROON);
     EndDrawing();
   }
 
