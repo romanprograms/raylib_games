da32a931a97196a3dd0b54fdea40355de07d85e8
Roman Nikolaenkov <romanliveshere@gmail.com>
2026-04-13 00:35:04 -0700

    refactor

--- snake_simple/src/main.c
+++ snake_simple/src/main.c
@@ -2,18 +2,18 @@
 #include "raylib.h"
 
 #define SNAKE_SEGMENT_SIZE 20
-#define INITIAL_SNAKE_SIZE 200;
-#define INITIAL_SPEED 5
+#define INITIAL_SNAKE_SIZE 20;
+#define SNAKE_ARBITRARY_SIZE_MAX 200
+#define INITIAL_SPEED 1
 #define SCREEN_WIDTH 2048
 #define SCREEN_HEIGHT 1024
 #define TARGET_FPS 60
-#define SNAKE_ARBITRARY_SIZE_MAX 200
 
 // TODO: do you need that ?
-const Vector2 HEAD_UP = {0.0f, -1.0f};
-const Vector2 HEAD_RIGHT = {1.0f, 0.0f};
-const Vector2 HEAD_DOWN = {0.0f, 1.0f};
-const Vector2 HED_LEFT = {-1.0f, 0.0f};
+const Vector2 UP_UNIT_VECTOR = {0.0f, -1.0f};
+const Vector2 RIGHT_UNIT_VECTOR = {1.0f, 0.0f};
+const Vector2 DOWN_UNIT_VECTOR = {0.0f, 1.0f};
+const Vector2 LEFT_UNIT_VECTOR = {-1.0f, 0.0f};
 
 typedef enum Direction
 {
@@ -23,11 +23,11 @@ typedef enum Direction
   RIGHT
 } Direction;
 
-typedef struct SnakePart
+typedef struct SnakeSegment
 {
-  Vector2 coords;
+  Vector2 position;
   Direction direction;
-} SnakePart;
+} SnakeSegment;
 
 typedef struct Snake
 {
@@ -35,31 +35,31 @@ typedef struct Snake
   // linked list, we're just gonna use a fixed size
   // array of sufficient length that we will make sure
   // to never exceed.
-  SnakePart parts[SNAKE_ARBITRARY_SIZE_MAX];
+  SnakeSegment parts[SNAKE_ARBITRARY_SIZE_MAX];
   size_t size;
-  size_t head_i;
-  size_t tail_i;
+  size_t head_idx;
+  size_t tail_idx;
   int speed;
 } Snake;
 
-void Snake_Init(Snake *s)
+void Snake__Init(Snake *s)
 {
   int head_x = SCREEN_WIDTH / 2;
 
   s->size = INITIAL_SNAKE_SIZE;
-  s->head_i = s->size - 1;
-  s->tail_i = s->head_i + 1 - s->size;
+  s->head_idx = s->size - 1;
+  s->tail_idx = s->head_idx + 1 - s->size;
   s->speed = INITIAL_SPEED;
 
   int tail_x = head_x - (s->size - 1) * SNAKE_SEGMENT_SIZE;
 
-  for (size_t i = 0; i <= s->head_i; i++)
+  for (size_t i = 0; i <= s->head_idx; i++)
   {
-    s->parts[i] = (SnakePart){(Vector2){tail_x + i * SNAKE_SEGMENT_SIZE, SCREEN_HEIGHT / 2}, (Direction)RIGHT};
+    s->parts[i] = (SnakeSegment){(Vector2){tail_x + i * SNAKE_SEGMENT_SIZE, SCREEN_HEIGHT / 2}, (Direction)RIGHT};
   }
 }
 
-Vector2 get_unit_speed_vector(Direction direction)
+Vector2 GetUnitSpeedVector(Direction direction)
 {
   Vector2 speed_unit_vector = {0};
   switch (direction)
@@ -91,48 +91,48 @@ Vector2 get_unit_speed_vector(Direction direction)
   return speed_unit_vector;
 }
 
-void Snake_ChangeHeadDirection(Snake *s, Direction direction)
+void Snake__ChangeHeadDirection(Snake *s, Direction direction)
 {
-  if (s->size <= 1 && direction != s->parts[s->head_i].direction)
+  if (s->size <= 1 && direction != s->parts[s->head_idx].direction)
   {
-    s->parts[s->head_i].direction = direction;
+    s->parts[s->head_idx].direction = direction;
     return;
   }
 
-  SnakePart head = s->parts[s->head_i];
-  SnakePart body_first_segment = s->parts[s->head_i - 1];
+  SnakeSegment head = s->parts[s->head_idx];
+  SnakeSegment neck = s->parts[s->head_idx - 1];
 
-  // only allow head to turn if the next segment already
+  bool hasHeadDirectionChanged = head.direction != direction;
+
+  // only allow head to turn if the neck already
   // follows the head in the same direction
-  if (head.direction == body_first_segment.direction && head.direction != direction)
+  if (head.direction == neck.direction && hasHeadDirectionChanged)
   {
-    s->parts[s->head_i].direction = direction;
+    s->parts[s->head_idx].direction = direction;
   }
 }
 
-void Snake_ChangeBodyDirection(Snake *s)
+void Snake__ChangeBodyDirection(Snake *s)
 {
-
   if (s->size > 1)
   {
-    for (size_t i = s->tail_i; i < s->head_i; i++)
+    // iterate over body segments only (i < s-> head_idx)
+    for (size_t i = s->tail_idx; i < s->head_idx; i++)
     {
 
       if (s->parts[i].direction == s->parts[i + 1].direction)
         continue;
 
       // determine if there's a direction change at the next segment
-      //  based on that direction determin whether the current segment
+      // based on that direction determin whether the current segment
       // should be x or y  aligned with the next segment
       // if the current segment is already aligned, change direction to
       // the direction of the next segment.
-      // if (s->parts[i].direction ->parts[i + 1])
-
       bool shouldTurnUpOrDown = (s->parts[i].direction == RIGHT || s->parts[i].direction == LEFT) &&
-                                (s->parts[i].coords.x == s->parts[i + 1].coords.x);
+                                (s->parts[i].position.x == s->parts[i + 1].position.x);
 
       bool shouldTurnLeftOrRight = (s->parts[i].direction == UP || s->parts[i].direction == DOWN) &&
-                                   (s->parts[i].coords.y == s->parts[i + 1].coords.y);
+                                   (s->parts[i].position.y == s->parts[i + 1].position.y);
 
       bool shouldTurnInTheDirectionOfTheNextSegment = shouldTurnLeftOrRight || shouldTurnUpOrDown;
 
@@ -144,54 +144,52 @@ void Snake_ChangeBodyDirection(Snake *s)
   }
 }
 
-void Snake_Move(Snake *s)
+void Snake__Move(Snake *s)
 {
   if (IsKeyDown(KEY_D) || IsKeyDown(KEY_RIGHT))
   {
-    Snake_ChangeHeadDirection(s, RIGHT);
+    Snake__ChangeHeadDirection(s, RIGHT);
   }
 
   if (IsKeyDown(KEY_W) || IsKeyDown(KEY_UP))
   {
-    Snake_ChangeHeadDirection(s, UP);
+    Snake__ChangeHeadDirection(s, UP);
   }
 
   if (IsKeyDown(KEY_S) || IsKeyDown(KEY_DOWN))
   {
-    Snake_ChangeHeadDirection(s, DOWN);
+    Snake__ChangeHeadDirection(s, DOWN);
   }
 
   if (IsKeyDown(KEY_A) || IsKeyDown(KEY_LEFT))
   {
-    Snake_ChangeHeadDirection(s, LEFT);
+    Snake__ChangeHeadDirection(s, LEFT);
   }
 
-  // When the snake moves one the distance
-  // equal to its segment size, reavaluate
-  // directions of each segment
-  Snake_ChangeBodyDirection(s);
+  Snake__ChangeBodyDirection(s);
 
   // move snake parts
-  for (size_t i = 0; i <= s->head_i; i++)
+  for (size_t i = 0; i <= s->head_idx; i++)
   {
-    Vector2 segment_unit_speed_vector = get_unit_speed_vector(s->parts[i].direction);
-    s->parts[i].coords.x += s->speed * segment_unit_speed_vector.x;
-    s->parts[i].coords.y += s->speed * segment_unit_speed_vector.y;
+    Vector2 segment_unit_speed_vector = GetUnitSpeedVector(s->parts[i].direction);
+    s->parts[i].position.x += s->speed * segment_unit_speed_vector.x;
+    s->parts[i].position.y += s->speed * segment_unit_speed_vector.y;
   }
 }
 
-void Snake_Draw(Snake *s)
+void Snake__Draw(Snake *s)
 {
-  for (size_t i = 0; i <= s->head_i; i++)
+  for (size_t i = 0; i <= s->head_idx; i++)
   {
-    DrawRectangle(s->parts[i].coords.x, s->parts[i].coords.y, SNAKE_SEGMENT_SIZE, SNAKE_SEGMENT_SIZE, MAROON);
-    // DrawRectangleLines(s->parts[i].coords.x + 1, s->parts[i].coords.y + 1, SNAKE_SEGMENT_SIZE - 1, SNAKE_SEGMENT_SIZE - 1, RAYWHITE);
+    DrawRectangle(s->parts[i].position.x, s->parts[i].position.y, SNAKE_SEGMENT_SIZE, SNAKE_SEGMENT_SIZE, MAROON);
+    // draws white border around a segment
+    // DrawRectangleLines(s->parts[i].position.x + 1, s->parts[i].position.y + 1, SNAKE_SEGMENT_SIZE - 1, SNAKE_SEGMENT_SIZE - 1, RAYWHITE);
   }
 }
 
 void Update(Snake *s)
 {
-  Snake_Move(s);
+  Snake__Move(s);
 }
 
 const char *DirectionToString(Direction direction)
@@ -211,15 +209,15 @@ const char *DirectionToString(Direction direction)
   }
 }
 
-void Snake_Stringify(Snake *s)
+void Snake__Log(Snake *s)
 {
   printf("\nSnake Info ================\n");
-  printf("Head Idx: %d, Tail Idx: %d, Size: %d, Speed: %d\n", s->head_i, s->tail_i, s->size, s->speed);
-  for (size_t i = s->tail_i; i <= s->head_i; i++)
+  printf("Head Idx: %d, Tail Idx: %d, Size: %d, Speed: %d\n", s->head_idx, s->tail_idx, s->size, s->speed);
+  for (size_t i = s->tail_idx; i <= s->head_idx; i++)
   {
     char *direction = DirectionToString(s->parts[i].direction);
     printf("\n Segment i: %d  x: %d y: %d direction: %s \n",
-           i, s->parts[i].coords.x, s->parts[i].coords.y, direction);
+           i, s->parts[i].position.x, s->parts[i].position.y, direction);
   }
   printf("\n=================\n");
 }
@@ -229,7 +227,7 @@ void Draw(Snake *s)
   ClearBackground(BLACK);
   BeginDrawing();
 
-  Snake_Draw(s);
+  Snake__Draw(s);
 
   EndDrawing();
 }
@@ -238,7 +236,7 @@ int main()
 {
   Snake s;
 
-  Snake_Init(&s);
+  Snake__Init(&s);
 
   InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Snake Game");
   SetTargetFPS(TARGET_FPS);
@@ -247,7 +245,7 @@ int main()
   {
     Update(&s);
     Draw(&s);
-    Snake_Stringify(&s);
+    Snake__Log(&s);
   }
 
   CloseWindow();
